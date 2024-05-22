e91f1aabbd2db6d67dfbb783f06332ade3ea8866
Gianluca Cerreta <gianlucacerreta@tv569b930ea014.station>
2024-05-22 19:02:58 +0200

    Inizio sviluppi User Story 3

--- aulab_post_cerreta/routes/web.php
+++ aulab_post_cerreta/routes/web.php
@@ -25,7 +25,7 @@ Route::get('/careers', [PublicController::class, 'careers'])->name('careers');
 Route::post('/careers/submit', [PublicController::class, 'careersSubmit'])->name('careers.submit');
 
 Route::middleware('admin')->group(function() {
-    Route::get('/admin/daashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
     Route::patch('/admin/{user}/set-admin', [AdminController::class, 'setAdmin'])->name('admin.setAdmin');
     Route::patch('/admin/{user}/set-revisor', [AdminController::class, 'setRevisor'])->name('admin.setRevisor');
     Route::patch('/admin/{user}/set-writer', [AdminController::class, 'setWriter'])->name('admin.setWriter');
